15505581ebd1bb8c2d7762733b877af552fbee28
Christopher Lampert <christopherlampert14@gmail.com>
2017-12-29 16:03:38 +0000

    started exercise 6

---
+++ Exercises/ex_6_string_lists.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""
+Ask the user for a string and print out whether this string is a palindrome or not.
+(A palindrome is a string that reads the same forwards and backwards.)
+"""
+
+def main():
+    pass
+
+
+if __name__ == '__main__': main()
